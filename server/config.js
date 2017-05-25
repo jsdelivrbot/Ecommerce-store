8a4e84db9c3ebbbb6ba87ffaeeee21d954030f1f
Nathan McCoy <nathanmccoy25@gmail.com>
2017-05-24 22:32:35 -0600

    changed over to non-local db

--- server/config.js
+++ server/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   secret: '123',
   clientID: '768657386649420',
-  clientSecret: 'fb60e2ed200db0374b5ce18789b06231'
+  clientSecret: 'fb60e2ed200db0374b5ce18789b06231',
+  mongoPassword: 'Clearwater_15'
 }
--- server/database.js
+++ server/database.js
@@ -2,7 +2,9 @@
 
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/store', (err) => {
+var config = require('./config.js')
+
+mongoose.connect('mongodb://nayray21:' + config.mongoPassword + '@cluster0-shard-00-00-2wvyg.mongodb.net:27017,cluster0-shard-00-01-2wvyg.mongodb.net:27017,cluster0-shard-00-02-2wvyg.mongodb.net:27017/shop?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin', (err) => {
   if(err){
     console.log("Failed to connect to database")
   }
