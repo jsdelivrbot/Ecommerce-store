23e3a9cb43ce5a4890e6f739589517444f7ee5ea
Nathan McCoy <nathanmccoy25@gmail.com>
2017-05-25 19:54:18 -0600

    connected to mongomLab

--- server/database.js
+++ server/database.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 
 var config = require('./config.js')
 
-mongoose.connect('mongodb://nayray21:' + config.mongoPassword + '@cluster0-shard-00-00-2wvyg.mongodb.net:27017,cluster0-shard-00-01-2wvyg.mongodb.net:27017,cluster0-shard-00-02-2wvyg.mongodb.net:27017/shop?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin', (err) => {
+mongoose.connect('mongodb://heroku_h3smw2qx:bim9mh5vtev01p50ggg0ak7unk@ds153521.mlab.com:53521/heroku_h3smw2qx', (err) => {
   if(err){
     console.log("Failed to connect to database")
   }
