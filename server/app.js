d0f82a29e413428d2327524d8bc3455bbd89dea6
Nathan McCoy <nathanmccoy25@gmail.com>
2017-05-25 21:30:47 -0600

    added another ../

--- server/app.js
+++ server/app.js
@@ -70,7 +70,7 @@ app.use(parser.json())
 app.use('/api', router)
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join( __dirname, '../build', 'index.html'))
+  res.sendFile(path.join( __dirname, '../../build', 'index.html'))
 })
 
 app.get('/auth/facebook', passport.authenticate('facebook'));
